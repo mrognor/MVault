1b687c6d7f4a39b02ec201c0ae0f677eb9377f3a
Ammaev Timur <timurlego1@yandex.ru>
2024-01-28 22:28:33 +0300

    Fix ToString

---
+++ Source/ToString.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+
+/**
+    \todo Добавить описание этой функции в доки
+    
+    \brief A template method for providing an interface converting any type to a string
+
+    This function allows you to use the same interface inside the DataStorage for any class, 
+    since this function accepts any type. Therefore, if you do not plan to use the functionality of saving to files, 
+    then there will be no problems when using a custom type. At the same time, if you plan to work with files, 
+    you can specialize this function for each required type.
+
+    \tparam <T> Any type of data
+    \param [in] data the variable to be converted to a string
+
+    \return This function returns an empty string. 
+    The specialization of this function for types will return strings with data from data.
+*/
+template <class T>
+std::string ToString(const T& data) { return ""; }
+
+/// \todo Добавить описание этой функции в доки
+template <>
+inline std::string ToString(const int& data) { return std::to_string(data); }
+
+/// \todo Добавить описание этой функции в доки
+template <>
+inline std::string ToString(const bool& data) { if(data) return "true"; else return "false"; }
+
+/// \todo Добавить описание этой функции в доки
+template <>
+inline std::string ToString(const std::string& data) { return data; }
+
+/// \todo Добавить описание этой функции в доки
+template <>
+inline std::string ToString(const float& data) { return std::to_string(data); }
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,5 +1,35 @@
 #include "DataStorage.h"
 
+template <>
+struct std::hash<std::vector<int>>
+{
+    std::size_t operator()(const std::vector<int>& vec) const
+    {
+        std::size_t seed = vec.size();
+  		for(auto& i : vec)
+    		seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+  		return seed;
+    }
+};
+
+/// \todo Добавить описание этой функции в доки
+template <>
+std::string ToString(const std::vector<int>& data) 
+{
+    if (data.size() == 0) return "{}";
+    std::string res = "{";
+    for (const int& it : data)
+    {
+        res += std::to_string(it) + ", ";
+    }
+
+    res.pop_back();
+    res.pop_back();
+
+    res += "}";
+    return res; 
+}
+
 int main()
 {
     DataStorage ds;
@@ -7,11 +37,12 @@ int main()
     ds.SetKey("id", -1);
     ds.SetKey<std::string>("name", "");
     ds.SetKey("gender", true);
+    ds.SetKey("slaves", std::vector<int>());
 
-    ds.CreateRecord({ {"id", 0}, {"name", std::string("A")}, {"gender", true}});
-    ds.CreateRecord({ {"id", 1}, {"name", std::string("B")}, {"gender", false}});
-    ds.CreateRecord({ {"id", 2}, {"name", std::string("C")}, {"gender", true}});
-    ds.CreateRecord({ {"id", 3}, {"name", std::string("D")}, {"gender", false}});
+    ds.CreateRecord({ {"id", 0}, {"name", std::string("A")}, {"gender", true} });
+    ds.CreateRecord({ {"id", 1}, {"name", std::string("B")}, {"gender", false} });
+    ds.CreateRecord({ {"id", 2}, {"name", std::string("C")}, {"gender", true} });
+    ds.CreateRecord({ {"id", 3}, {"name", std::string("D")}, {"gender", false}, {"slaves", std::vector<int>{1, 2, 3}} });
     ds.PrintAsTable();
 
     DataStorageRecordRef dsrr = ds.GetRecord("id", 2);
