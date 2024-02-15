15743731adc581cbd378c85c27dc3963f9142a55
Ammaev Timur <timurlego1@yandex.ru>
2024-02-15 20:40:24 +0300

    Update windows compatibility

--- Docs/Gendoc.bat
+++ Docs/Gendoc.bat
@@ -1,6 +1,11 @@
 :: Add main page doxygen command to ReadMe.md
 cd /d %~dp0
 
+:: Create directory with examples
+cp -rf ../Examples/ ./
+powershell -Command "(gc Examples/SortExample.cpp) -replace '// startcode', '/** @code' | Out-File -encoding ASCII Examples/SortExample.cpp"
+powershell -Command "(gc Examples/SortExample.cpp) -replace '// endcode', '@endcode */' | Out-File -encoding ASCII Examples/SortExample.cpp"
+
 :: Create documentation
 doxygen Doxyfile
 
