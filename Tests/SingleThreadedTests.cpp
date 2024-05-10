acf192b573f114fad804d6b657ed9198b2a888c0
Ammaev Timur <timurlego1@yandex.ru>
2024-05-10 14:13:29 +0300

    Update tests

--- Tests/SingleThreadedTests.cpp
+++ Tests/SingleThreadedTests.cpp
@@ -12,40 +12,70 @@ void KeyAddictionTests()
     TEST_ASSERT(vlt.GetKeys() == vector<string>(), "Vault is not created with empty keys");
 
     // Create VaultRecordRefs
-    VaultRecordRef vrr1, vrr2;
-    TEST_ASSERT(vrr1.GetKeys() == vector<string>(), "VaultRecordRef is not created with empty keys");
+    VaultRecordRef vrr;
+    TEST_ASSERT(vrr.GetKeys() == vector<string>(), "VaultRecordRef is not created with empty keys");
 
     // Create VaultRecordSets
-    VaultRecordSet vrs1, vrs2;
-    TEST_ASSERT(vrs1.GetKeys() == vector<string>(), "VaultRecordSet is not created with empty keys");
+    VaultRecordSet vrs;
+    TEST_ASSERT(vrs.GetKeys() == vector<string>(), "VaultRecordSet is not created with empty keys");
 
     // Create new record
-    vrr1 = vlt.CreateRecord();
+    vrr = vlt.CreateRecord();
 
     // Add key
     vlt.AddKey<int>("A", -1);
 
     // Check VaultRecordRef dynamic key addition
-    TEST_ASSERT(vrr1.GetKeys() == vector<string>({"A"}), "The key is not added dynamically to VaultRecordRef");
+    TEST_ASSERT(vrr.GetKeys() == vector<string>({"A"}), "The key is not added dynamically to VaultRecordRef");
 
     // Request VaultRecordSet
-    vlt.RequestEqual("A", -1, vrs1);
+    vlt.RequestEqual("A", -1, vrs);
 
     // Check VaultRecordSet request
-    TEST_ASSERT(vrs1.GetKeys() == vector<string>({"A"}), "The VaultRecordSet request is not working correctly");
+    TEST_ASSERT(vrs.GetKeys() == vector<string>({"A"}), "The VaultRecordSet request is not working correctly");
 
     // Add keys
     vlt.AddKey<std::string>("B", "none");
     vlt.AddKey<bool>("C", false);
 
     // Check dynamic key addition
-    TEST_ASSERT(vrr1.GetKeys() == vector<string>({"A", "B", "C"}), "The key is not added dynamically to VaultRecordRef");
-    TEST_ASSERT(vrs1.GetKeys() == vector<string>({"A", "B", "C"}), "The key is not added dynamically to VaultRecordSet");
-    vrs1.GetRecord<std::string>("B", "none", vrr1);
-    TEST_ASSERT(vrr1.IsValid(), "The key is not added dynamically to VaultRecordSet");
+    TEST_ASSERT(vrr.GetKeys() == vector<string>({"A", "B", "C"}), "The key is not added dynamically to VaultRecordRef");
+    TEST_ASSERT(vrs.GetKeys() == vector<string>({"A", "B", "C"}), "The key is not added dynamically to VaultRecordSet");
+    vrs.GetRecord<std::string>("B", "none", vrr);
+    TEST_ASSERT(vrr.IsValid(), "The key is not added dynamically to VaultRecordSet");
+}
+
+void KeyUpdationTests()
+{
+    // Create Vault
+    Vault vlt;
+    
+    // Create VaultRecordRef
+    VaultRecordRef vrr;
+
+    // Add key
+    vlt.AddKey<int>("A", -1);
+
+    // Create record
+    vrr = vlt.CreateRecord();
+
+    // Check VaultRecordRef getting
+    int A;
+    vrr.GetData("A", A);
+    TEST_ASSERT(A == -1, "The default setting does not work correctly when creating a record");
+
+    // Update default value
+    vlt.UpdateKey("A", 0);
+    vrr = vlt.CreateRecord();
+    vlt.GetRecord("A", 0, vrr);
+
+    // Check key value updating
+    vrr.GetData("A", A);
+    TEST_ASSERT(A == 0, "Updating the default key value does not work correctly");
 }
 
 int main()
 {
     KeyAddictionTests();
+    KeyUpdationTests();
 }
